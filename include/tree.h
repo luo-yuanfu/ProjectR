3048853a331fd61cac39594e0478a1e594f40fc9
Luo Yuanfu <varellow@gmail.com>
2015-10-06 00:37:33 +0800

    add Connie's part

--- include/tree.h
+++ include/tree.h
@@ -4,6 +4,8 @@
 #define MAX_TREE_DEPTH 8
 #define SAMPLE_UV_NUM 100
 #define SAMPLE_THRESHOLD_NUM 50
+#define UV_RANGE 10
+#define MEANSHIFT
 
 #include "node.h"
 #include "image_table.h"
@@ -16,15 +18,15 @@ public:
 
 	Tree();
 	~Tree();
+	void FreeNode(Node* cur_node);
 
 	//calculate feature value for pixel using u and v
-	double FeatureValue(Pixel piexl, int u, int v);
+	double FeatureValue(Pixel piexl, std::pair<int,int> u, std::pair<int,int> v);
 	void InitRoot(vector <Pixel> pixels);
 	void ConstructTree();
 	void BuildTree(int depth, Node* cur_node);//build the tree, without calculating the labels for the leaf nodes
-	void CalLeafLabel(); //calculate the labels for the leaf nodes
-	void RandomSample(int &u, int &v);
-	void RandomSample(double &threshold);
+	void CalLeafLabel(Node* cur_node); //calculate the labels for the leaf nodes
+	void RandomSample(std::pair<int,int> &u, std::pair<int,int> &v);
 	double InformationGain(Node *cur_node, vector<Pixel> left_pixel, vector<Pixel> right_pixel);
 
 	void set_image_table(ImageTable* image_table);
